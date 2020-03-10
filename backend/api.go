97dbb34bb1212722e3df890e87f998038d1b4be4
Lars Jellema <lars.jellema@gmail.com>
2020-03-10 13:36:10 +0100

    Initial commit

---
+++ backend/api.go
@@ -0,0 +1,59 @@
+package backend
+
+import "fmt"
+import "log"
+import "net/http"
+
+import _ "golang.org/x/net/websocket"
+import "github.com/privacybydesign/irmago"
+import "github.com/privacybydesign/irmago/server"
+
+type Configuration struct {
+	IrmaServerURL       string
+	ServicePhoneNumber  string
+	PurposeToAttributes map[string]irma.AttributeConDisCon
+}
+
+func (cfg Configuration) generateDTMF() string {
+	return "00000000" // TODO
+}
+
+func (cfg Configuration) irmaRequest(purpose string, dtmf string) (irma.RequestorRequest, error) {
+	condiscon, ok := cfg.PurposeToAttributes[purpose]
+	if !ok {
+		return nil, fmt.Errorf("Unknown call purpose: %#v", purpose)
+	}
+
+	disclosure := irma.NewDisclosureRequest()
+	disclosure.Disclose = condiscon
+	disclosure.ClientReturnURL = "tel:" + cfg.ServicePhoneNumber + dtmf
+
+	request := &irma.ServiceProviderRequest{
+		Request: disclosure,
+	}
+
+	return request, nil
+}
+
+// A citizen pressed the call with Irma button on a page on the Gemeente
+// Nijmegen website in order to start a new calling Irma session. The citizen
+// frontend makes a POST request to the backend with only one piece of
+// information: The subject the citizen wants to ask about. The backend looks up
+// what Irma attributes the agent will need and asks the Irma server for a new
+// Irma session. The backend generates a DTMF code and responds with a JSON
+// object with a valid Irma session response with a tel return url containing
+// the DTMF code.
+func (cfg Configuration) handleNewSession(w http.ResponseWriter, r *http.Request) {
+	purpose := r.FormValue("purpose")
+	dtmf := cfg.generateDTMF()
+	request, err := cfg.irmaRequest(purpose, dtmf)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	transport := irma.NewHTTPTransport(cfg.IrmaServerURL)
+	var pkg server.SessionPackage
+	err = transport.Post("session", pkg, request)
+}
