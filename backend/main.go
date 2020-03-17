a3d0330f70c848b730482bd8671908a2ab30f44f
Henk Dieter Oordt <henkdieter@oordt.net>
2020-03-17 14:46:36 +0100

    undo commenting expireDaemon

--- backend/main.go
+++ backend/main.go
@@ -92,7 +92,7 @@ func main() {
 
 	// TODO: Fail immediately if configured Irma server or configured database
 	// can't be reached before entering ListenAndServe.
-	// go expireDaemon(cfg)
+	go expireDaemon(cfg)
 
 	http.HandleFunc("/call", cfg.handleCall)
 	http.HandleFunc("/session", cfg.handleSession)
