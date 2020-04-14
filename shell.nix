4dd5c2fdf009ea21c041e3606440b85724ef3b2f
Lars Jellema <lars.jellema@gmail.com>
2020-04-14 15:03:23 +0200

    Remove vgo2nix dependency

--- shell.nix
+++ shell.nix
@@ -1,6 +1,5 @@
 let
   sources = import nix/sources.nix;
   pkgs = import sources.nixpkgs { };
-  vgo2nix = import sources.vgo2nix { inherit pkgs; };
 
-in pkgs.mkShell { buildInputs = with pkgs; [ go vgo2nix ]; }
+in pkgs.mkShell { buildInputs = with pkgs; [ go ]; }
