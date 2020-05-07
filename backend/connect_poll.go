7f362ce3e86552b394a6a8434cab2e467a1147cf
Lars Jellema <lars.jellema@gmail.com>
2020-05-07 20:29:41 +0200

    Use TouchFeed in connectPollDaemon

--- backend/connect_poll.go
+++ backend/connect_poll.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"time"
 )
@@ -11,35 +10,29 @@ import (
 // Polls Amazon Connect continuously and sends status updates
 // to all listeners.
 func connectPollDaemon(cfg Configuration) {
-	pollTicker := time.NewTicker(1000 * time.Millisecond)
-	dbTicker := time.NewTicker(1000 * time.Millisecond)
-
-	for {
-
-		select {
-		case <-pollTicker.C:
-			response, err := cfg.getConnectCurrentMetrics()
-
-			if err != nil {
-				log.Printf("Connect poll could not get Amazon Connect metrics")
-				continue
-			}
-
-			msg, err := json.Marshal(response)
-
-			if err != nil {
-				log.Printf("failed to marshal amazon connect metrics: %v", err)
-				continue
-			}
-
-			cfg.db.Notify("kcc", "amazon-connect", string(msg))
-		case <-dbTicker.C:
-			count, err := cfg.db.activeSessionCount()
-			if err != nil {
-				log.Printf("Could not get active session count: %v", err)
-				break
-			}
-			cfg.db.Notify("kcc", "active-sessions", fmt.Sprintf("%d", count))
+	ticker := time.NewTicker(time.Second)
+	for range ticker.C {
+		ours, err := cfg.db.TouchFeed("kcc")
+		if err != nil {
+			log.Printf("failed to update feed in database: %v", err)
+			return
+		} else if !ours {
+			// This feed is no longer our responsibility.
+			return
 		}
+
+		response, err := cfg.getConnectCurrentMetrics()
+		if err != nil {
+			log.Printf("Connect poll could not get Amazon Connect metrics")
+			continue
+		}
+
+		msg, err := json.Marshal(response)
+		if err != nil {
+			log.Printf("failed to marshal amazon connect metrics: %v", err)
+			continue
+		}
+
+		cfg.db.Notify("kcc", "amazon-connect", string(msg))
 	}
 }
