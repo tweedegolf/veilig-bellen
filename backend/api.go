272e006c2c2e778d02f7343434c37bd277706c8f
Henk Dieter Oordt <henkdieter@oordt.net>
2020-08-25 14:49:01 +0200

    update session status to 'unavailable' if lambda call passes a 'call_status' parameter with  value 'unavailable'

--- backend/api.go
+++ backend/api.go
@@ -252,7 +252,9 @@ func (cfg Configuration) handleSessionStatus(w http.ResponseWriter, r *http.Requ
 // related Amazon Lambda but not from the internet.
 func (cfg Configuration) handleCall(w http.ResponseWriter, r *http.Request) {
 	dtmf := r.FormValue("dtmf")
-	connectUnavailable := r.FormValue("unavailable")
+	callState := r.FormValue("call_state")
+
+	log.Printf("call_state: %v", callState)
 
 	secret, err := cfg.db.secretFromDTMF(dtmf)
 
@@ -261,9 +263,10 @@ func (cfg Configuration) handleCall(w http.ResponseWriter, r *http.Request) {
 	} else if err != nil {
 		log.Printf("failed to retrieve secret from dtmf: %v", err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
-	} else if connectError != "" {
+	} else if callState == "unavailable" {
 		cfg.db.setStatus(secret, "UNAVAILABLE")
 		log.Printf("Amazon connect was not available")
+		io.WriteString(w, "OK")
 	} else {
 		io.WriteString(w, secret)
 		cfg.db.setStatus(secret, "CALLED")
